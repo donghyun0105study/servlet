ae72c27805ffb300a8c94f9617ed39f9f11b8c6b
팜하니 <donghyun0105.study@gmail.com>
2023-09-19 12:22:57 +0900

    Feat : Hello 서블릿.

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -2,9 +2,30 @@ package hello.servlet.basic;
 
 
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.rmi.ServerException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
-public class HelloServlet {
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServerException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
 
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
 
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        try {
+            response.getWriter().write("hello " + username);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
