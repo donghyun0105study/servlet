fb35156cee10ac25a8770ffe11d4aa1fd36ae547
donghyun0105study <138381584+donghyun0105study@users.noreply.github.com>
2023-09-24 12:49:11 +0900

    Feat : HttpServletResponse - 기본 사용법.

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -2,6 +2,7 @@ package hello.servlet.basic.response;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -21,7 +22,32 @@ public class ResponseHeaderServlet extends HttpServlet {
         resp.setHeader("Pragma", "no-cache");
         resp.setHeader("my-header", "hello");
 
+        // Header 편의 메서드.
+//        content(resp);
+//        cookie(resp);
+//        redirect(resp);
+
+        // message body.
         PrintWriter writer = resp.getWriter();
         writer.println("OK");
     }
+
+    private void content(HttpServletResponse response) {
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("myCookie", "good");
+        cookie.setMaxAge(600);// 600초.
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) throws IOException {
+//        response.setStatus(HttpServletResponse.SC_FOUND);
+//        response.setHeader("Location", "/basic/hello-form.html");
+        response.sendRedirect("/basic/hello-form.html");
+    }
+
+
 }
